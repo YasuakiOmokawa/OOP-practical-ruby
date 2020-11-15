8908740267b6b78a7a73c7d8b9ce5a8288bb26d5
面川泰明 <omokawa@senk-inc.co.jp>
2020-11-15 18:55:48 +0900

    Add keyword parameter

--- main.rb
+++ main.rb
@@ -4,6 +4,6 @@ require './lib/Wheel'
 @wheel = Wheel.new(26, 1.5)
 puts @wheel.circumference
 
-puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(chainring: 52, cog: 11, wheel: @wheel).gear_inches
 
-puts Gear.new(52, 11).ratio
+puts Gear.new(chainring: 52, cog: 11).ratio
