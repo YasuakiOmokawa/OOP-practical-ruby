688c33e14b48a257421d616bf06089d8431b95a9
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-15 11:57:37 +0900

    Change keyword argument to hash argument

--- lib/Bicycle.rb
+++ lib/Bicycle.rb
@@ -1,10 +1,10 @@
 class Bicycle
   attr_reader :size, :chain, :tire_size
 
-  def initialize(size:, chain: default_chain, tire_size: default_tire_size)
-    @size = size
-    @chain = chain
-    @tire_size = tire_size
+  def initialize(args={})
+    @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
   end
 
   def spares
