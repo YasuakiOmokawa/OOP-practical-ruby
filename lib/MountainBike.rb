1c4c650224cae2ff00e5d0b1531bab64122a7833
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-15 11:28:34 +0900

    Create bikes

---
+++ lib/MountainBike.rb
@@ -0,0 +1,19 @@
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(size:, chain:, tire_size:, front_shock:, rear_shock:)
+    @front_shock = front_shock
+    @rear_shock = rear_shock
+    super(size: size, chain: chain, tire_size: tire_size)
+  end
+
+  def spares
+    super.merge({
+      rear_shock: rear_shock
+    })
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-require './lib/Gear'
-require './lib/Wheel'
+Dir[File.expand_path("../lib/", __FILE__) << '/*.rb'].each do | file |
+  require file
+end
 
 @wheel = Wheel.new(rim: 26, tire: 1.5)
 puts @wheel.circumference
 
 puts Gear.new(chainring: 52, cog: 11, wheel: @wheel).gear_inches
-
 puts Gear.new(chainring: 52, cog: 11).ratio
+puts RoadBike.new(size: 'S', tape_color: 'red').spares
