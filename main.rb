64524baedc1355f0bf2ed44316d6073f48044e60
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-21 11:59:30 +0900

    Renew main test to fix compose

--- main.rb
+++ main.rb
@@ -21,15 +21,22 @@ v.schedulable?(starting, ending)
 m = Mechanic.new
 m.schedulable?(starting, ending)
 
+chain = Part.new(name: 'chain', description: '10-speed')
+road_tire = Part.new(name: 'tire_size', description: '23')
+tape = Part.new(name: 'tape_color', description: 'red')
+mountain_tire = Part.new(name: 'tire_size', description: '2.1')
+rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
+front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
+road_bike_parts = Parts.new([chain, road_tire, tape])
 road_bike = Bicycle.new(
   size: 'L',
-  parts: RoadBikeParts.new(tape_color: 'red')
+  parts: Parts.new([chain, road_tire, tape])
 )
 p road_bike.size
 p road_bike.spares
 mountain_bike = Bicycle.new(
   size: 'S',
-  parts: MountainBikeParts.new(rear_shock: 'Fox')
+  parts: Parts.new([chain, mountain_tire, front_shock, rear_shock])
 )
 p mountain_bike.size
 p mountain_bike.spares
