bed2d55876c1e09af82afa1e89ccbe22141f0259
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-28 13:48:48 +0900

    Description diameterizable role

--- test/WheelTest.rb
+++ test/WheelTest.rb
@@ -1,10 +1,18 @@
 require 'minitest/autorun'
-require '../lib/Wheel'
+require_relative '../lib/Wheel'
 
 class WheelTest < MiniTest::Test
 
+  def setup
+    @wheel = Wheel.new(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel, :diameter)
+  end
+
   def test_calculates_diameter
-    wheel = Wheel.new(rim: 26, tire: 1.5)
+    wheel = Wheel.new(26, 1.5)
 
     assert_in_delta(29, wheel.diameter, 0.01)
   end
