14ab1b123b1e6692be844f9b4886966b02678f87
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-28 13:39:07 +0900

    Add DiameterDouble

--- test/GearTest.rb
+++ test/GearTest.rb
@@ -2,12 +2,17 @@ require 'minitest/autorun'
 require_relative '../lib/Gear'
 require_relative '../lib/Wheel'
 
+class DiameterDouble
+  def diameter
+    10
+  end
+end
 class GearTest < MiniTest::Test
 
   def test_calculates_gear_inches
     gear = Gear.new(chainring: 52, cog: 11,
-      wheel: Wheel.new(26, 1.5))
+      wheel: DiameterDouble.new)
 
-    assert_in_delta(137.1, gear.gear_inches, 0.01)
+    assert_in_delta(47.27, gear.gear_inches, 0.01)
   end
 end
