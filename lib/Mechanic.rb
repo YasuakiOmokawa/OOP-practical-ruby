9d809a554810c818d0c4c7ab2cab2bddd01d4f55
面川泰明 <omokawa@senk-inc.co.jp>
2020-12-31 10:09:57 +0900

    Add prepare_trip interface
    fix #33

--- lib/Mechanic.rb
+++ lib/Mechanic.rb
@@ -1,6 +1,12 @@
 class Mechanic
   include Schedulable
 
+  def prepare_trip(trip)
+    trip.bicycles.each {|bicycle|
+      prepare_bicycle(bicycle)
+    }
+  end
+
   def lead_days
     4
   end
--- lib/TripCordinator.rb
+++ lib/TripCordinator.rb
@@ -1,4 +1,9 @@
 class TripCordinator
+
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+
   def buy_food(customers)
     #...
   end
