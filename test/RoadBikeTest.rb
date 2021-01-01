bbee46845de8de71bb993f9dcaac24388cf70d1f
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:39:13 +0900

    Add test of practice subclass

--- test/RoadBikeTest.rb
+++ test/RoadBikeTest.rb
@@ -9,6 +9,10 @@ class RoadBikeTest < MiniTest::Test
   include BicycleSubclassTest
 
   def setup
-    @bike = @object = RoadBike.new
+    @bike = @object = RoadBike.new(tape_color: 'red')
+  end
+
+  def test_puts_tape_color_in_local_spares
+    assert_equal 'red', @bike.local_spares[:tape_color]
   end
 end
