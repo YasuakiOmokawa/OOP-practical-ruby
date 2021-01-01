25330f3157ed4cee5faed98df40b80f225065d8a
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:04:50 +0900

    Create Bicycle interface test module

---
+++ test/module/BicycleInterfaceTest.rb
@@ -0,0 +1,13 @@
+module BicycleInterfaceTest
+  def test_responds_to_default_tire_size
+    assert_respond_to(@object, :default_tire_size)
+  end
+
+  def test_responds_to_default_chain
+    assert_respond_to(@object, :default_chain)
+  end
+
+  def test_responds_to_chain
+    assert_respond_to(@object, :chain)
+  end
+end
