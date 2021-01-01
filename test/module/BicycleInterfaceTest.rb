b936db8e7df471fa789e031f641faf3371b68fcd
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:06:58 +0900

    Add rest of interface test to test module

--- test/module/BicycleInterfaceTest.rb
+++ test/module/BicycleInterfaceTest.rb
@@ -10,4 +10,16 @@ module BicycleInterfaceTest
   def test_responds_to_chain
     assert_respond_to(@object, :chain)
   end
+
+  def test_responds_to_size
+    assert_respond_to(@object, :size)
+  end
+
+  def test_responds_to_tire_size
+    assert_respond_to(@object, :tire_size)
+  end
+
+  def test_responds_to_spares
+    assert_respond_to(@object, :spares)
+  end
 end
