54e46f25812f5349ad91741b6e819d78728c8950
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:31:34 +0900

    Add test to force subclass implement

--- test/BicycleTest.rb
+++ test/BicycleTest.rb
@@ -8,4 +8,8 @@ class BicycleTest < MiniTest::Test
   def setup
     @bike = @object = Bicycle.new({tire_size: 0})
   end
+
+  def test_forces_subclass_to_implement_default_tire_size
+    assert_raises(NotImplementedError) {@bike.default_tire_size}
+  end
 end
