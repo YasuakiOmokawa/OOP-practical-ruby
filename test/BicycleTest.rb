b5abbe4f23b733ab8ce8cb34e1151b6a0ca7b681
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:44:21 +0900

    Add stubbed class to test abstract super class

--- test/BicycleTest.rb
+++ test/BicycleTest.rb
@@ -2,14 +2,28 @@ require 'minitest/autorun'
 require_relative './module/BicycleInterfaceTest'
 require_relative '../lib/Bicycle'
 
+class StubbedBike < Bicycle
+  def default_tire_size
+    0
+  end
+  def local_spares
+    {saddle: 'painful'}
+  end
+end
 class BicycleTest < MiniTest::Test
   include BicycleInterfaceTest
 
   def setup
     @bike = @object = Bicycle.new({tire_size: 0})
+    @stubbed_bike = StubbedBike.new
   end
 
   def test_forces_subclass_to_implement_default_tire_size
     assert_raises(NotImplementedError) {@bike.default_tire_size}
+  end
+
+  def test_includes_local_spares_in_spares
+    assert_equal @stubbed_bike.spares,
+      {tire_size: 0, chain: '10-speed', saddle: 'painful'}
   end
 end
