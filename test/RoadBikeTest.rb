330d9e11a16f8729792615416b24be206f3fd659
面川泰明 <omokawa@senk-inc.co.jp>
2021-01-01 14:22:27 +0900

    Imprement subclass interface test

--- test/RoadBikeTest.rb
+++ test/RoadBikeTest.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
 require_relative './module/BicycleInterfaceTest'
+require_relative './module/BicycleSubclassTest'
 require_relative '../lib/Bicycle'
 require_relative '../lib/RoadBike'
 
 class RoadBikeTest < MiniTest::Test
   include BicycleInterfaceTest
+  include BicycleSubclassTest
 
   def setup
     @bike = @object = RoadBike.new
